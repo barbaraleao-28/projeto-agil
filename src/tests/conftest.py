62b19a1e28126b30d4740236c4881215c990b1b6
barbaraleao-28 <barbararodriguesleao@gmail.com>
2025-11-09 15:16:18 -0300

    Update conftest.py

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,16 +1,9 @@
 import pytest
-import sys
-import os
-
-# Adiciona a pasta 'src' ao caminho de importação do Python
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-# Importa o app do seu arquivo app.py
+# IMPORTAÇÃO CORRETA, POIS O CI AGORA SABE ONDE ENCONTRAR 'src'
 from src.app import app 
 
 @pytest.fixture
 def client():
-    # ... (Resto do código do fixture)
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
